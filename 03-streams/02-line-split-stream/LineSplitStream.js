1f8ca83d5d8460342e6fe8de728c115baf47b516
Ilya Zuev <zuev@banki.ru>
2020-11-03 15:20:23 +0300

    add lineSplitStream implementation for 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -7,9 +7,22 @@ class LineSplitStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, callback) {
+    const lastLine = this.lastLine !== null && this.lastLine !== undefined ? this.lastLine : '';
+    const lines = (lastLine + chunk.toString()).split(os.EOL);
+
+    this.lastLine = lines.pop();
+
+    lines.forEach(line => {
+      this.push(line)
+    });
+
+    callback();
   }
 
   _flush(callback) {
+    const lastLine = this.lastLine !== null && this.lastLine !== undefined ? this.lastLine : '';
+    this.push(lastLine);
+    callback()
   }
 }
 
